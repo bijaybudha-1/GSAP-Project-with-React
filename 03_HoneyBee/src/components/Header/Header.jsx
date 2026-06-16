596e37efd360c3d2e9900511a32895c2bfa9c538
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-06-16 16:54:21 +0545

    Modify navbar text weight

--- 03_HoneyBee/src/components/Header/Header.jsx
+++ 03_HoneyBee/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { RiMenu4Line, RiUser3Line } from "@remixicon/react";
 const Header = () => {
     return (
         <header className="w-full h-[10vh] px-20 bg-[#009b46] flex justify-between items-center fixed z-40">
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-5 font-bold text-xl">
                 <img
                     className="h-12 mr-4"
                     src="./src/assets/Logo.png"
@@ -12,7 +12,7 @@ const Header = () => {
                 <h2>About me</h2>
                 <h2>Contact us</h2>
             </div>
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-5 text-xl">
                 <RiUser3Line />
                 <RiMenu4Line />
             </div>
